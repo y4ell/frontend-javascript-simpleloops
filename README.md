8321cb32abe4a07fca37d5da6204031a376e6e49
novaeeken <nova_eeken@hotmail.com>
2023-03-13 13:46:38 +0100

    Create assignment

---
+++ README.md
@@ -0,0 +1,16 @@
+# Opdracht beschrijving
+
+Deze opdracht bestaat uit meerdere opdrachten van oplopende moeilijkheidsgraad om zoveel mogelijk met het schrijven van simpele loops te kunnen oefenen. Je kunt deze opdracht maken door het project te clonen of downloaden naar jouw eigen computer via deze GitHub repository. De uitwerkingen staan op de branch _uitwerkingen_.
+
+Dit project bevat de volgende bestanden:
+
+1. `loops.js`
+
+Schrijf onder iedere vraag de JavaScript code die gevraagd wordt en check overal met een `console.log()` of het klopt.
+
+## Script runnen
+Als je de code wil runnen kun je dit doen door het volgende in de terminal in te voeren:
+
+`nodemon loops.js`
+
+![loop meme](http://www.quickmeme.com/img/54/540c1f0a68918de905cadeb944fdea46c1f1fe9c8808512d88fe1866beae02ef.jpg)
---
+++ loops.js
@@ -0,0 +1,97 @@
+// ==========================================
+// Opdracht 1. Maak een for-loop die 3 keer "Hoera!" in de terminal logt
+// Verwachte uitkomst:
+// Hoera!
+// Hoera!
+// Hoera!
+// ==========================================
+
+
+
+
+
+// ==========================================
+// Opdracht 2. Maak een for-loop die tijdens iedere herhaling de waarde van i logt in de zin: "De waarde van i is [nummer]"
+// Verwachte uitkomst:
+// De waarde van i is: 0
+// De waarde van i is: 1
+// De waarde van i is: 2
+// De waarde van i is: 3
+// De waarde van i is: 4
+// ==========================================
+
+
+
+
+
+// ==========================================
+// Opdracht 3a. Maak een for-loop die de waarde van i logt. De loop begint bij 2 (!) en loopt tot en met 10.
+// Verwachte uitkomst:
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// 10
+// ==========================================
+
+
+
+
+
+// ==========================================
+// Opdracht 3b. Gebruik dezelfde for-loop uit 3a, maar log nu de waarde van i vermenigvuldigd met 7.
+// Verwachte uitkomst:
+// 14
+// 21
+// 28
+// 35
+// 42
+// 49
+// 56
+// 63
+// 70
+// ==========================================
+
+
+
+
+
+// ==========================================
+// Opdracht 3c. Gebruik dezelfde for-loop uit 3b, maar zorg dat de volgende zin wordt gelogd: "Wanneer een hond [x] jaar is, staat dat gelijk aan [y] mensenjaren." Tip: 1 hondenjaar staat gelijk aan 7 mensenjaren.
+// Verwachte uitkomst:
+// Wanneer een hond 2 jaar is, staat dat gelijk aan 14 mensenjaren.
+// Wanneer een hond 3 jaar is, staat dat gelijk aan 21 mensenjaren.
+// Wanneer een hond 4 jaar is, staat dat gelijk aan 28 mensenjaren.
+// Wanneer een hond 5 jaar is, staat dat gelijk aan 35 mensenjaren.
+// Wanneer een hond 6 jaar is, staat dat gelijk aan 42 mensenjaren.
+// Wanneer een hond 7 jaar is, staat dat gelijk aan 49 mensenjaren.
+// Wanneer een hond 8 jaar is, staat dat gelijk aan 56 mensenjaren.
+// Wanneer een hond 9 jaar is, staat dat gelijk aan 63 mensenjaren.
+// Wanneer een hond 10 jaar is, staat dat gelijk aan 70 mensenjaren.
+// ==========================================
+
+
+
+
+
+// ==========================================
+// Bonusopdracht. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal EVEN of ONEVEN is.
+// Verwachte uitkomsten:
+// 0 is even
+// 1 is oneven
+// 2 is even
+// 3 is oneven
+// .... etc.
+// 15 is oneven
+
+// Tip 1: Het is mogelijk om beslissingsstructuren te gebruiken binnen de {} van een for-loop!
+// Tip 2: Een getal is EVEN als je het deelt door 2 en er blijft niets over... Doet dit nog geen belletje rinkelen? Google dan eens naar 'MDN webdocs remainder operator'.
+// ==========================================
+
+
+
+
